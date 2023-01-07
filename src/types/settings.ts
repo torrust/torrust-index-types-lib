2338e613cb1662c2666c8ab4fadd18d24c5c5d79
Warm Beer <mickvd99@gmail.com>
2023-01-07 16:50:14 +0100

    chore: updated types

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,8 +1,44 @@
-export class Settings {
-    website_name: string;
-    tracker_url: string;
-    tracker_mode: TrackerMode;
-    email_on_signup: Requirement;
+export type Settings = {
+    website: {
+        name: string
+    },
+    tracker: {
+        url: string
+        mode: TrackerMode
+        api_url: string
+        token: string
+        token_valid_seconds: number
+    },
+    net: {
+        port: number
+        base_url: null
+    },
+    auth: {
+        email_on_signup: Requirement,
+        min_password_length: number
+        max_password_length: number
+        secret_key: string
+    },
+    database: {
+        connect_url: string
+        torrent_info_update_interval: number
+    },
+    mail: {
+        email_verification_enabled: boolean
+        from: string
+        reply_to: string
+        username: string
+        password: string
+        server: string
+        port: number
+    }
+}
+
+export type PublicSettings = {
+    website_name: string
+    tracker_url: string
+    tracker_mode: TrackerMode
+    email_on_signup: Requirement
 }
 
 export enum TrackerMode {
--- src/types/torrent.ts
+++ src/types/torrent.ts
@@ -1,27 +1,40 @@
-export class Torrent {
-    torrent_id: number;
-    uploader: string;
-    info_hash: string;
-    title: string;
-    description: string;
-    category: TorrentCategory;
-    upload_date: string;
-    file_size: number;
-    seeders: number;
-    leechers: number;
-    files: Array<TorrentFile>;
-    trackers: Array<string>;
-    magnet_link: string;
+export type Torrent = {
+    torrent_id: number
+    uploader: string
+    info_hash: string
+    title: string
+    description: string
+    category: TorrentCategory
+    upload_date: string
+    file_size: number
+    seeders: number
+    leechers: number
+    files: Array<TorrentFile>
+    trackers: Array<string>
+    magnet_link: string
+}
+
+export type TorrentCompact = {
+    category_id: number
+    date_uploaded: string
+    description: string
+    file_size: number
+    info_hash: string
+    leechers: number
+    seeders: number
+    title: string
+    torrent_id: number
+    uploader: string
 }
 
 export type TorrentCategory = {
-    category_id: number;
-    name: string;
-    num_torrents: number;
+    category_id: number
+    name: string
+    num_torrents: number
 }
 
 export type TorrentFile = {
-    path: Array<string>;
-    length: number;
-    md5sum: number;
+    path: Array<string>
+    length: number
+    md5sum: number
 }
