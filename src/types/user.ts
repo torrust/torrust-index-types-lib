777670af8f9bd1b306a7a430ea00c84add295e3b
Mario <mariomnt945@gmail.com>
2025-01-16 02:51:20 +0100

    feat: [#28] new type for user profiles

--- src/types/user.ts
+++ src/types/user.ts
@@ -3,3 +3,12 @@ export type TokenResponse = {
     username: string
     admin: boolean
 }
+
+export type UserProfile = {
+    user_id: number
+    username: string
+    email: string
+    email_verified: boolean
+    bio: string
+    avatar: string
+}
